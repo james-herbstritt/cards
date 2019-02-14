7a9f64960332c7f4da7de2c4d754ed3ad5f996d5
James Herbstritt <james.herbstritt@gmail.com>
2019-02-14 16:21:11 -0500

    Create chip enum and files for class player and hand

---
+++ hand.py
@@ -0,0 +1,9 @@
+## a hand of a game has:
+## a list of players
+## an amount of cards to deal
+## a pot of chips
+## a winner (can be multiple players in super edge cases)
+
+
+
+## just something to remember, check if the players have chips to play
---
+++ player.py
@@ -0,0 +1,5 @@
+## a player should have:
+## a hand of cards (represented by a list of cards)
+## a group of chips
+## a decision (call, bet, fold)
+
